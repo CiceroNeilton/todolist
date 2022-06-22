4c889e6e0a8cdec27c5bfd6cbf88837aa1bfd110
CiceroNeilton <neilton961@gmail.com>
2022-06-22 19:33:37 -0300

    first commit

---
+++ app/Http/Controllers/Todolist2Controller.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use App\Models\Todolist2;
+use Illuminate\Http\Request;
+
+class Todolist2Controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        try {
+            $tasks = Todolist2::all();
+            return response()->json($tasks, 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+   
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            //$todolist = new todolist();
+            //$todolist->title = $request->title;
+            //$todolist->description = $request->description;
+            //$todolist->deadline = $request->deadline;
+            //$todolist->save();
+            Todolist2::create($request->all());
+            return response()->json(['message' => 'Tarefa criada com sucesso'], 200);
+        } catch (\Exception ) {
+            return response()->json(['message' => 'Erro ao criar tarefa'], 500);
+        }
+        return response()->json(['message' => 'Vou salvar os dados'], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $todolist = Todolist2::findOrFail($id);
+            $todolist->title = $request->title;
+            $todolist->description = $request->description;
+            $todolist->deadline = $request->deadline;
+            $todolist->save();
+            Todolist2::create($request->all());
+            return response()->json(['message' => 'Tarefa atualizada com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Erro ao atualizar tarefa'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            $todolist = Todolist2::findOrFail($id);
+            $todolist ->delete();
+            return response()->json(['message' => 'Tarefa deletada com sucesso'], 200);
+        } catch (\Exception $e) {
+            return response()->json(['message' => 'Erro ao deletar tarefa: ' .$e->getMessage()], 500);
+        }
+    }
+}
